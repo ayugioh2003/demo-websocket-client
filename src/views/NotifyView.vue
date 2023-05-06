26a1fc137458fc347da0816392f265ed034812f4
ayugioh2003 <ayugioh2003@gmail.com>
2023-05-06 18:53:58 +0800

    feat: send message when user in & out

--- src/views/NotifyView.vue
+++ src/views/NotifyView.vue
@@ -1,9 +1,32 @@
 <script setup>
-import Notify from '../components/Notify.vue'
+import { onMounted, onUnmounted } from 'vue'
+import io from 'socket.io-client'
+import Swal from 'sweetalert2'
+
+let socket = {}
+
+onMounted(() => {
+  const URL = 'http://localhost:3005/'
+  socket = io(URL, { transports: ['websocket'] })
+
+  socket.on('userConnectNotify', (data) => {
+    console.log('data from server:', data)
+    Swal.fire({
+      toast: true,
+      title: data,
+      position: 'top-end',
+      showConfirmButton: false,
+      timerProgressBar: true,
+      timer: 3000
+    })
+  })
+})
+
+onUnmounted(() => {
+  socket?.disconnect()
+})
 </script>
 
 <template>
-  <main>
-    <Notify />
-  </main>
+  <div>1. 有使用者進入或離開頁面，跳出提示通知</div>
 </template>
